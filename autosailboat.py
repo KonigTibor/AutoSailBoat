583e4a2d92ec23ed7d4375f52cf869a914ae567d
Kőnig Tibor <konig.tibor@hotmail.hu>
2017-01-21 11:38:15 +0100

    Added pitch and roll calculation.

--- autosailboat.py
+++ autosailboat.py
@@ -4,6 +4,7 @@
 # Author: Tony DiCola
 # License: Public Domain
 import time
+import math
 
 # Import the LSM303 module.
 from libraries.Adafruit_LSM303 import LSM303
@@ -21,7 +22,9 @@ while True:
     # Grab the X, Y, Z components from the reading and print them out.
     accel_x, accel_y, accel_z = accel
     mag_x, mag_z, mag_y = mag
-    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
-          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
+    pitch = math.atan(accel_x / math.sqrt(accel_y^2 + accel_z^2))
+    roll = math.atan(accel_y / math.sqrt(accel_x^2 + accel_z^2))
+    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}, Pitch={6}, Roll={7}'.format(
+          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z, pitch, roll))
     # Wait half a second and repeat.
     time.sleep(0.5)
